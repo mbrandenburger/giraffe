08ca7f6950a78934108b61a66652a72cee1d4334
fbahr <florian.bahr@gmail.com>
2012-12-03 10:55:28 +0100

    *** v0.1 revamped ***

--- giraffe/client/giraffe-client.py
+++ giraffe/client/giraffe-client.py
@@ -2,44 +2,71 @@ __author__  = 'fbahr'
 __version__ = '0.1'
 __date__    = '2012-12-02'
 
-from cement.core import backend, foundation, hook # < cement 2.0.2
-import requests                                   # < requests 0.14.2
+from cement.core import backend, foundation, controller, handler # < cement 2.0.2
+import requests                                                  # < requests 0.14.2
+
+# default config options
+# defaults = backend.defaults('giraffe-client')
+# defaults['giraffe-client']['debug']       = False
+# defaults['giraffe-client']['auth_url']    = None # OS_AUTH_URL
+# defaults['giraffe-client']['username']    = None # OS_USERNAME
+# defaults['giraffe-client']['password']    = None # OS_PASSWORD
+# defaults['giraffe-client']['tenant_id']   = None
+# defaults['giraffe-client']['tenant_name'] = None
 
 
-# set default config options
-defaults = backend.defaults('giraffe-client')
-defaults['giraffe-client']['debug'] = False
-defaults['giraffe-client']['auth'] = None # OS_AUTH_URL
-defaults['giraffe-client']['username'] = None # OS_USERNAME
-defaults['giraffe-client']['password'] = None # OS_PASSWORD
-defaults['giraffe-client']['tenant_id']  = None
-defaults['giraffe-client']['tenantname'] = None
+# an application base controller
+class BaseController(controller.CementBaseController):
+    class Meta:
+        label = 'base-controller'
+        # description = ''
+
+        # default config options
+        config_defaults = dict(
+            auth        = None, # OS_AUTH_URL
+            username    = None, # OS_USERNAME
+            password    = None, # OS_PASSWORD
+            tenant_id   = None,
+            tenant_name = None
+            )
+
+        # command line arguments
+        arguments = [
+            (['-a',     '--auth_url'],    dict(action='store', help='OS_AUTH_URL')),
+            (['-u',     '--username'],    dict(action='store', help='OS_USERNAME')),
+            (['-p',     '--password'],    dict(action='store', help='OS_PASSWORD')),
+            (['-tid',   '--tenant_id'],   dict(action='store', help='OS_TENANT_ID')),
+            (['-tname', '--tenant_name'], dict(action='store', help='OS_TENANT_NAME'))
+            ]
+        #   ...
+        #   (['-F',      '--FLAG'],       dict(action='store_true', help='...'))
+        #   ...
+        
+        @controller.expose(help='')
+        def GET(self):
+            # self.log.info("Inside base.command1 function.")
+                r = requests.get("",
+                     auth=(app.pargs.user, app.pargs.passname))
+
+
+class GiraffeClient(foundation.CementApp):
+    class Meta:
+        label = 'giraffe-client'
+        # description = ''        
+        base_controller = BaseController()
+
+
+    def __init__(self, **kwargs):
+        foundation.CementApp.__init(kwargs)
+
 
 # create an application
-app = foundation.CementApp('giraffe-client', config_defaults=defaults)
-
+app = GiraffeClient('giraffe-client', config_defaults=defaults)
 
 try:
     # setup the application
     app.setup()
-
-    # add arguments
-    app.args.add_argument('-a', '--auth_url', action='store', metavar='',
-                          help='OS_AUTH_URL')
-    app.args.add_argument('-u', '--username', action='store', metavar='',
-                          help='OS_USERNAME')
-    app.args.add_argument('-p', '--password', action='store', metavar='',
-                          help='OS_PASSWORD')
-    app.args.add_argument('-tid', '--tenant_id', action='store', metavar='',
-                          help='OS_TENANT_ID')
-    app.args.add_argument('-tname', '--tenantname', action='store', metavar='',
-                          help='OS_TENANT_NAME')
-
     app.run()
-
-    # application logic    
-    r = requests.get("",
-                     auth=(app.pargs.user, app.pargs.passname))
 
 finally:
     # close the application

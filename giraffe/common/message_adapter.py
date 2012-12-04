7fda219c2a6966213ec30b256e49d8adf651c777
Marcus <m.brandenburger@tu-bs.de>
2012-12-04 19:52:07 +0100

    Add timestamp

--- giraffe/common/message_adapter.py
+++ giraffe/common/message_adapter.py
@@ -16,16 +16,19 @@ class MessageAdapter(object):
 
 #        (Marcus)TODO: Whats about duration? Must depent on metering duration
         record1 = hostMsg.meters.add()
+        record1.timestamp = "jetzt"
         record1.type = "CPU_AVG_1"
         record1.value = str(params[0])
         record1.duration = 60
 
         record2 = hostMsg.meters.add()
+        record2.timestamp = "jetzt"
         record2.type = "CPU_AVG_5"
         record2.value = str(params[1])
         record2.duration = 300
 
         record3 = hostMsg.meters.add()
+        record3.timestamp = "jetzt"
         record3.type = "CPU_AVG_15"
         record3.value = str(params[2])
         record3.duration = 1500

f94812d66b0117eafa8317583feed092bb6e3d5f
Marcus <m.brandenburger@tu-bs.de>
2012-12-08 14:58:03 +0100

    add config parameter and timestamp

--- giraffe/common/message_adapter.py
+++ giraffe/common/message_adapter.py
@@ -1,6 +1,9 @@
-from giraffe.common import Message_pb2
-
 __author__ = 'marcus'
+
+from giraffe.common import Message_pb2
+from giraffe.common.config import Config
+
+from datetime import datetime
 
 # (Marcus)TODO: Adapter bauen
 
@@ -10,25 +13,27 @@ class MessageAdapter(object):
     @staticmethod
     def host_cpu_avg(params):
 
+        time_now = str(datetime.now())
+
         hostMsg = Message_pb2.MeterHostMessage()
 #        (Marcus)TODO: replace MacBook with real hostname from config file
-        hostMsg.hostID = "MacBook"
+        hostMsg.hostID = Config().get("agent", "hostname")
 
 #        (Marcus)TODO: Whats about duration? Must depent on metering duration
         record1 = hostMsg.meters.add()
-        record1.timestamp = "jetzt"
+        record1.timestamp = time_now
         record1.type = "CPU_AVG_1"
         record1.value = str(params[0])
         record1.duration = 60
 
         record2 = hostMsg.meters.add()
-        record2.timestamp = "jetzt"
+        record2.timestamp = time_now
         record2.type = "CPU_AVG_5"
         record2.value = str(params[1])
         record2.duration = 300
 
         record3 = hostMsg.meters.add()
-        record3.timestamp = "jetzt"
+        record3.timestamp = time_now
         record3.type = "CPU_AVG_15"
         record3.value = str(params[2])
         record3.duration = 1500

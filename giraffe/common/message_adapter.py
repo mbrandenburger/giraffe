bf25829f63f77ad1001d89e649a627b7d1dd02cc
Oliver <oliverdm@example>
2012-12-14 22:56:16 +0100

    MessageAdapter updated, new protobuf message structure

--- giraffe/common/message_adapter.py
+++ giraffe/common/message_adapter.py
@@ -1,15 +1,72 @@
-__author__ = 'marcus'
+
 
 from giraffe.common import Message_pb2
+from giraffe.common import BulkMessage_pb2
 from giraffe.common.config import Config
 
 from datetime import datetime
 
-# (Marcus)TODO: Adapter bauen
 
 class MessageAdapter(object):
+    """
+    MessageAdapter exposes all attributes of the underlying adaptee object
+    so all attributes of that object can be accessed by calling
+    adapter.adaptee_attribute.
+    """
 
-#    TODO: Factory
+    def __init__(self, adaptee=None):
+        # need to use self.__dict__ to avoid infinite recursion in __getattr__
+        if adaptee is not None:
+            self.__dict__['adaptee'] = adaptee
+        else:
+            self.__dict__['adaptee'] = BulkMessage_pb2.BulkMessage()
+
+    def __getattr__(self, name):
+        if hasattr(self.adaptee, name):
+            return self.adaptee.__getattribute__(name)
+        else:
+            raise AttributeError(('Neither adapter nor adaptee has the '
+                                  'attribute "%s"') % name)
+
+    def __setattr__(self, name, value):
+        if hasattr(self.adaptee, name):
+            self.adaptee.__setattr__(name, value)
+        else:
+            raise AttributeError(('Neither adapter nor adaptee has the '
+                                  'attribute "%s"') % name)
+
+    def add_host_record(self, timestamp, meter_type, value, duration):
+        """
+        Adds a host record to the underlying adaptee object.
+        The host_id has to be set by accessing the attribute directory, i.e.
+        by calling adapter.host_id = '...'
+        """
+        if self.adaptee.host_records is None:
+            self.adaptee.host_records = []
+
+        host_record = self.adaptee.host_records.add()
+        host_record.timestamp = timestamp
+        host_record.type = meter_type
+        host_record.value = value
+        host_record.duration = duration
+
+    def add_instance_record(self, project_id, user_id, instance_id, timestamp,
+                          meter_type, value, duration):
+        """
+        Adds an instance record to the underlying adaptee object.
+        """
+        if self.adaptee.instance_records is None:
+            self.adaptee.instance_records = []
+        instance_record = self.adaptee.instance_records.add()
+        instance_record.project_id = project_id
+        instance_record.user_id = user_id
+        instance_record.instance_id = instance_id
+        instance_record.timestamp = timestamp
+        instance_record.type = meter_type
+        instance_record.value = value
+        instance_record.duration = duration
+
+    # @deprecated: use add_host_record() and add_instance_record() instead
     @staticmethod
     def host_cpu_avg(params):
 
---
+++ test/common/test_message_adapter.py
@@ -0,0 +1,64 @@
+from datetime import datetime
+import unittest
+
+from giraffe.common.message_adapter import MessageAdapter
+from giraffe.common import BulkMessage_pb2
+
+
+class MessageAdapterTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        super(MessageAdapterTestCase, self).setUp()
+
+    def tearDown(self):
+        super(MessageAdapterTestCase, self).tearDown()
+
+    def timestamp(self):
+        return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    def test_init_empty(self):
+        adapter = MessageAdapter()
+        self.assertIsNotNone(adapter.adaptee)
+
+    def test_init_message(self):
+        msg = BulkMessage_pb2.BulkMessage()
+        adapter = MessageAdapter(msg)
+        self.assertEqual(adapter.adaptee, msg)
+
+    def test_setattr(self):
+        adapter = MessageAdapter()
+        adapter.signature = 'fake_signature'
+        self.assertEqual(adapter.adaptee.signature, 'fake_signature')
+
+    def test_getattr(self):
+        adapter = MessageAdapter()
+        adapter.host_id = 'fake_host_id'
+        self.assertEqual(adapter.host_id, 'fake_host_id')
+
+    def test_add_host_record(self):
+        adapter = MessageAdapter()
+        self.assertEqual(len(adapter.host_records), 0)
+        adapter.add_host_record(timestamp=self.timestamp(),
+                                meter_type='fake_meter_type',
+                                value='10',
+                                duration=0)
+        self.assertGreater(len(adapter.host_records), 0)
+
+    def test_add_instance_record(self):
+        adapter = MessageAdapter()
+        self.assertEqual(len(adapter.instance_records), 0)
+        adapter.add_instance_record(project_id='fake_project_id',
+                                    user_id='fake_user_id',
+                                    instance_id='fake_instance_id',
+                                    timestamp=self.timestamp(),
+                                    meter_type='fake_meter_type',
+                                    value='20',
+                                    duration=0)
+        self.assertGreater(len(adapter.instance_records), 0)

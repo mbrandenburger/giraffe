b12f81b86a76a1496903e1d8aa87407365e151b5
Oliver <oliverdm@example>
2012-12-15 20:06:04 +0100

    Create table script added

---
+++ bin/db_create_tables.sh
@@ -0,0 +1,30 @@
+import os
+import sys
+
+possible_topdir = os.path.normpath(os.path.join(os.path.abspath(
+        sys.argv[0]), os.pardir, os.pardir))
+
+if os.path.exists(os.path.join(possible_topdir, "giraffe", "__init__.py")):
+    sys.path.append(possible_topdir)
+
+from giraffe.common.config import Config
+import giraffe.service.db as db
+from giraffe.service.db import Base
+
+print 'Loading configuration.........',
+config = Config("giraffe.cfg")
+print '\tdone'
+
+print 'Connecting to database........',
+db = db.connect('%s://%s:%s@%s/%s' % (config.get('db', 'vendor'),
+                                      config.get('db', 'user'),
+                                      config.get('db', 'pass'),
+                                      config.get('db', 'host'),
+                                      config.get('db', 'schema')))
+print '\tdone'
+
+print 'Creating tables...............',
+Base.metadata.create_all(db._engine)
+print '\tdone'
+
+print '\nAll done - tables successully created!'

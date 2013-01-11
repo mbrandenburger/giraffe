649918651fc69d3cda811a03e4fb70fa7e79c915
Oliver <oliverdm@example>
2013-01-11 22:38:36 +0100

    Separation of rest_api from flask

--- giraffe/service/rest_api.py
+++ giraffe/service/rest_api.py
@@ -2,8 +2,7 @@ import json
 import logging
 import threading
 import re
-from functools import wraps
-from flask import Flask, Response, request
+import giraffe.service.rest_server as srv
 from giraffe.common.config import Config
 import giraffe.service.db as db
 from giraffe.service.db import Host, Meter, MeterRecord
@@ -30,80 +29,25 @@ class Rest_API(threading.Thread):
                                     _config.get('db', 'pass'),
                                     _config.get('db', 'host'),
                                     _config.get('db', 'schema')))
-
-        self.app = Flask(__name__)
-
-        def requires_auth(f):
-            return self.__requires_auth(f)
-
-        @self.app.route('/')
-        def welcome():
-            return self.__welcome()
-
-        @self.app.route('/hosts')
-        @self.app.route('/hosts/')
-        @requires_auth
-        def hosts():
-            return self.__hosts()
-
-        @self.app.route('/projects')
-        @self.app.route('/projects/')
-        @requires_auth
-        def projects():
-            return self.__projects()
-
-        @self.app.route('/users')
-        @self.app.route('/users/')
-        @requires_auth
-        def users():
-            return self.__users()
-
-        @self.app.route('/instances')
-        @self.app.route('/instances/')
-        @requires_auth
-        def instances():
-            return self.__instances()
-
-        @self.app.route('/hosts/<host_id>/meters/<meter_id>')
-        @self.app.route('/hosts/<host_id>/meters/<meter_id>/')
-        @requires_auth
-        def hosts_hid_meters_mid(host_id, meter_id):
-            return self.__hosts_hid_meters_mid(host_id, meter_id)
-
-        @self.app.route('/projects/<project_id>/meters/<meter_id>')
-        @self.app.route('/projects/<project_id>/meters/<meter_id>/')
-        @requires_auth
-        def projects_pid_meters_mid(project_id, meter_id):
-            return self.__projects_pid_meters_mid(project_id, meter_id)
-
-        @self.app.route('/users/<user_id>/meters/<meter_id>')
-        @self.app.route('/users/<user_id>/meters/<meter_id>/')
-        @requires_auth
-        def users_pid_meters_mid(user_id, meter_id):
-            return self.__users_uid_meters_mid(user_id, meter_id)
-
-        @self.app.route('/instances/<instance_id>/meters/<meter_id>')
-        @self.app.route('/instances/<instance_id>/meters/<meter_id>/')
-        @requires_auth
-        def instances_iid_meters_mid(instance_id, meter_id):
-            return self.__instances_iid_meters_mid(instance_id, meter_id)
-
+        self.server = None
         threading.Thread.__init__(self)
 
     def run(self):
-        self.app.run(host=_config.get('flask', 'host'),
-                     port=_config.getint('flask', 'port'))
+        self.server = srv.start(self, host=_config.get('flask', 'host'),
+                                      port=_config.getint('flask', 'port'),
+                                      user=_config.get('flask', 'user'),
+                                      password=_config.get('flask', 'pass'))
 
     def stop(self):
         self._Thread__stop()
 
-    def __query_params(self):
+    def __query_params(self, query_string):
         """
         Returns the query parameters for the current request as a dictionary.
         Values of the format "YYYY-MM-DD_HH-ii-ss" are converted to the format
         "YYYY-MM-DD HH:ii:ss".
         """
-        query_parts = request.query_string.split('&')
+        query_parts = query_string.split('&')
         params = {}
         for query_part in query_parts:
             parts = query_part.split('=', 2)
@@ -121,31 +65,10 @@ class Rest_API(threading.Thread):
                 continue
         return params
 
-    def __requires_auth(self, f):
-        def __check_auth(username, password):
-            """
-            checks whether a username/password combination is valid
-            """
-            return (username == _config.get('flask', 'user') and
-                    password == _config.get('flask', 'pass'))
+    def route_root(self):
+        return 'Welcome to Giraffe REST API'
 
-        @wraps(f)
-        def decorated(*args, **kwargs):
-            auth = request.authorization
-            if not (auth and __check_auth(auth.username, auth.password)):
-                """
-                sends a 401 response that enables basic auth
-                """
-                return Response({'message': 'Unauthorized'}, 401,
-                                {'WWW-Authenticate':
-                                 'Basic realm="REST API Login"'})
-            return f(*args, **kwargs)
-        return decorated
-
-    def __welcome(self):
-        return Response(response='Welcome to Giraffe REST API', status=200)
-
-    def __hosts(self):
+    def route_hosts(self):
         """
         Route: hosts
         """
@@ -154,7 +77,7 @@ class Rest_API(threading.Thread):
         self.db.session_close()
         return json.dumps([host.to_dict() for host in hosts])
 
-    def __projects(self):
+    def route_projects(self):
         """
         Route: projects
         """
@@ -166,7 +89,7 @@ class Rest_API(threading.Thread):
         self.db.session_close()
         return json.dumps(values)
 
-    def __users(self):
+    def route_users(self):
         """
         Route: users
         """
@@ -178,7 +101,7 @@ class Rest_API(threading.Thread):
         self.db.session_close()
         return json.dumps(values)
 
-    def __instances(self):
+    def route_instances(self):
         """
         Route: instances
         """
@@ -190,32 +113,41 @@ class Rest_API(threading.Thread):
         self.db.session_close()
         return json.dumps(values)
 
-    def ___hosts_hid(self):
+    def route_meters(self):
+        """
+        Route: meters
+        """
+        self.db.session_open()
+        meters = self.db.load(Meter, order='asc', order_attr='name')
+        self.db.session_close()
+        return json.dumps([meter.to_dict() for meter in meters])
+
+    def route_hosts_hid(self):
         """
         Route: hosts/<host_id>
         """
         return 'TODO'
 
-    def __hosts_hid_meters_mid(self, host_id, meter_id):
+    def route_hosts_hid_meters_mid(self, host_id, meter_id, query_string=''):
         """
         Route: hosts/<host_id>/meters/<meter_id>
         """
         self.db.session_open()
         hosts = self.db.load(Host, {'name': host_id}, limit=1)
         if not hosts:
-            return Response(response='host_name not found', status=404)
+            return None
         host = hosts[0]
 
         meters = hosts = self.db.load(Meter, {'name': meter_id}, limit=1)
         if not meters:
-            return Response(response='meter_name not found', status=404)
+            return None
         meter = meters[0]
 
         # narrow down the search
         limit = self.RESULT_LIMIT
         order = 'asc'
         search_params = {'host_id': host.id, 'meter_id': meter.id}
-        query_params = self.__query_params()
+        query_params = self.__query_params(query_string)
         if self.PARAM_START_TIME in query_params or self.PARAM_END_TIME in query_params:
             search_params['timestamp'] = ['0000-01-01 00:00:00',
                                           '2999-12-31 23:59:59']
@@ -230,21 +162,21 @@ class Rest_API(threading.Thread):
         self.db.session_close()
         return json.dumps([r.to_dict() for r in records])
 
-    def __projects_pid_meters_mid(self, project_id, meter_id):
+    def route_projects_pid_meters_mid(self, project_id, meter_id, query_string=''):
         """
         Route: projects/<project_id>/meters/<meter_id>
         """
         self.db.session_open()
         meters = self.db.load(Meter, {'name': meter_id}, limit=1)
         if not meters:
-            return Response(response='meter_name not found', status=404)
+            return None
         meter = meters[0]
 
         # narrow down the search
         limit = self.RESULT_LIMIT
         order = 'asc'
         search_params = {'project_id': project_id, 'meter_id': meter.id}
-        query_params = self.__query_params()
+        query_params = self.__query_params(query_string)
         if self.PARAM_START_TIME in query_params or self.PARAM_END_TIME in query_params:
             search_params['timestamp'] = ['0000-01-01 00:00:00',
                                           '2999-12-31 23:59:59']
@@ -259,21 +191,21 @@ class Rest_API(threading.Thread):
         self.db.session_close()
         return json.dumps([r.to_dict() for r in records])
 
-    def __users_uid_meters_mid(self, user_id, meter_id):
+    def route_users_uid_meters_mid(self, user_id, meter_id, query_string=''):
         """
         Route: users/<user_id>/meters/<meter_id>
         """
         self.db.session_open()
         meters = self.db.load(Meter, {'name': meter_id}, limit=1)
         if not meters:
-            return Response(response='meter_name not found', status=404)
+            return None
         meter = meters[0]
 
         # narrow down the search
         limit = self.RESULT_LIMIT
         order = 'asc'
         search_params = {'user_id': user_id, 'meter_id': meter.id}
-        query_params = self.__query_params()
+        query_params = self.__query_params(query_string)
         if self.PARAM_START_TIME in query_params or self.PARAM_END_TIME in query_params:
             search_params['timestamp'] = ['0000-01-01 00:00:00',
                                           '2999-12-31 23:59:59']
@@ -288,21 +220,21 @@ class Rest_API(threading.Thread):
         self.db.session_close()
         return json.dumps([r.to_dict() for r in records])
 
-    def __instances_iid_meters_mid(self, instance_id, meter_id):
+    def route_instances_iid_meters_mid(self, instance_id, meter_id, query_string=''):
         """
         Route: instances/<instance_id>/meters/<meter_id>
         """
         self.db.session_open()
         meters = self.db.load(Meter, {'name': meter_id}, limit=1)
         if not meters:
-            return Response(response='meter_name not found', status=404)
+            return None
         meter = meters[0]
 
         # narrow down the search
         limit = self.RESULT_LIMIT
         order = 'asc'
         search_params = {'resource_id': instance_id, 'meter_id': meter.id}
-        query_params = self.__query_params()
+        query_params = self.__query_params(query_string)
         if self.PARAM_START_TIME in query_params or self.PARAM_END_TIME in query_params:
             search_params['timestamp'] = ['0000-01-01 00:00:00',
                                           '2999-12-31 23:59:59']

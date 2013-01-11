efdbd4e8543bb9105fd6e0a2fbc285f5bcfa6282
Marcus <m.brandenburger@tu-bs.de>
2013-01-11 19:36:01 +0100

    change to pep8 style

--- giraffe/agent/host_meter.py
+++ giraffe/agent/host_meter.py
@@ -3,19 +3,23 @@ import os
 import psutil
 from giraffe.common.task import PeriodicMeterTask
 
+
 class Host_CPU_AVG(PeriodicMeterTask):
     def meter(self):
         avg = os.getloadavg()
         return avg
+
 
 class Host_UNAME(PeriodicMeterTask):
     def meter(self):
         uname = os.uname()
         return uname
 
+
 class Host_PHYMEM_Usage(PeriodicMeterTask):
     def meter(self):
         return psutil.phymem_usage()
+
 
 class Host_VIRTMEM_Usage(PeriodicMeterTask):
     def meter(self):

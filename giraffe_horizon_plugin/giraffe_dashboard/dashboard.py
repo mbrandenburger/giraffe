9f5c6ca47f1293fde0b3835d65c9068880a193b8
Oliver <oliverdm@example>
2013-01-19 21:24:57 +0100

    Dashboard progress (PanelGroups) and small fixes

--- giraffe_horizon_plugin/giraffe_dashboard/dashboard.py
+++ giraffe_horizon_plugin/giraffe_dashboard/dashboard.py
@@ -1,30 +1,29 @@
-# vim: tabstop=4 shiftwidth=4 softtabstop=4
-
-# Copyright 2012 Openstack, LLC
-#
-#    Licensed under the Apache License, Version 2.0 (the "License"); you may
-#    not use this file except in compliance with the License. You may obtain
-#    a copy of the License at
-#
-#         http://www.apache.org/licenses/LICENSE-2.0
-#
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#    License for the specific language governing permissions and limitations
-#    under the License.
-
 from django.utils.translation import ugettext as _
 
 import horizon
 
 
+class BasePanels(horizon.PanelGroup):
+    slug = "giraffe_info"
+    name = _("Giraffe Information")
+    panels = ('overview', 'hosts',)
+
+
+class SpecialPanels(horizon.PanelGroup):
+    slug = "special"
+    name = _("Special Information")
+    # note: the comma at the end DOES make a difference -
+    # without it, a PanelGroup with only one panel is not displayed!
+    panels = ('cats',)
+
+
 class GiraffePlugin(horizon.Dashboard):
     name = _("Giraffe")
     slug = "giraffe_dashboard"
-    panels = ('hosts', 'cats',)
-    default_panel = 'cats'
-    nav = True
+    # note: the comma at the end DOES make a difference -
+    # without it, the last PanelGroup is not displayed at all!
+    panels = (BasePanels, SpecialPanels,)
+    default_panel = 'overview'
 
 
 horizon.register(GiraffePlugin)

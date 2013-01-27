65f64f5f5845d7c7f354bc744f917dfd288b545a
fbahr <florian.bahr@gmail.com>
2013-01-27 11:38:25 +0100

    Initial commit of test/agent/test_inst_meters.py

---
+++ test/agent/test_inst_meters.py
@@ -0,0 +1,35 @@
+__author__ = 'fbahr'
+
+import sys
+import unittest
+import logging
+from giraffe.agent.instance_meter import Inst_PHYMEM_Usage
+
+
+class InstMeterTestCases(unittest.TestCase):
+
+    python_version = int(''.join([str(i) for i in sys.version_info[0:3]]))
+
+    logger = logging.getLogger("service.collector.instance_meters")
+    logger.setLevel(logging.DEBUG)
+    h = logging.StreamHandler()
+    h.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    logger.addHandler(h)
+
+
+    @classmethod
+    def setUpClass(cls):
+        if InstMeterTestCases.python_version >= 270:
+            cls.inst_phymem_usage = Inst_PHYMEM_Usage(None, 0)
+
+    def setUp(self):
+        if InstMeterTestCases.python_version < 270:
+            self.inst_phymem_usage = Inst_PHYMEM_Usage(None, 0)
+
+    def test_inst_phymem_usage(self):
+        meters = self.inst_phymem_usage.meter()
+        self.assertTrue(meters)
+
+
+if __name__ == '__main__':
+    unittest.main()

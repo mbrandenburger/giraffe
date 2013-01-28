98d198ebaf44f7017e6f0e6887ebef373b30d60c
Marcus <m.brandenburger@tu-bs.de>
2013-01-28 13:23:42 +0100

    keystone auth example

---
+++ test/common/KeystoneCurl.py
@@ -0,0 +1,60 @@
+__author__ = 'marcus'
+
+import requests
+import simplejson as json
+import keystoneclient.middleware.auth_token as auth
+
+
+"""
+    post user credentials and get token
+"""
+auth_url = 'http://cloud2.ibr.cs.tu-bs.de:35357/v2.0/tokens'
+auth_header = {'Content-type':'application/json'}
+auth_data = {'auth' :
+                 {'passwordCredentials':
+                      {'username': '',
+                       'password': ''}
+                 }
+            }
+r = requests.post(url=auth_url, data=json.dumps(auth_data, indent=4), headers=auth_header)
+print json.dumps(r.json, indent=4)
+
+parsed_data = json.loads(r.text)
+unscoped_token = str(parsed_data['access']['token']['id'])
+
+"""
+    get tenents
+"""
+auth_url = 'http://cloud2.ibr.cs.tu-bs.de:5000/v2.0/tenants'
+auth_header = {'X-Auth-Token':unscoped_token}
+r = requests.post(url=auth_url, data={}, headers=auth_header)
+print json.dumps(r.json, indent=4)
+
+
+
+"""
+    post user credentials with tenant and get token
+"""
+auth_url = 'http://cloud2.ibr.cs.tu-bs.de:35357/v2.0/tokens'
+auth_header = {'Content-type':'application/json'}
+auth_data = {'auth' :
+                 {'tenantName':'users',
+                  'token':{
+                      'id':unscoped_token
+                  }
+                 }
+}
+r = requests.post(url=auth_url, data=json.dumps(auth_data, indent=4), headers=auth_header)
+print json.dumps(r.json, indent=4)
+
+parsed_data = json.loads(r.text)
+token = str(parsed_data['access']['token']['id'])
+
+"""
+    check token
+"""
+auth_url = 'http://cloud2.ibr.cs.tu-bs.de:35357/v2.0/tokens/' + token
+auth_header = {'X-Auth-Token': ''}
+r = requests.get(url=auth_url, headers=auth_header)
+print r.text
+print json.dumps(r.json, indent=4)

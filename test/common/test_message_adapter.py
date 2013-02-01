445531bf66bbfcbeab72b1ce30ccdeb7be06448d
fbahr <florian.bahr@gmail.com>
2013-02-01 16:58:07 +0100

    test_message_adapter.py: update

--- test/common/test_message_adapter.py
+++ test/common/test_message_adapter.py
@@ -1,11 +1,15 @@
 from datetime import datetime
 import unittest
 
+import sys
 from giraffe.common.message_adapter import MessageAdapter
 from giraffe.common import BulkMessage_pb2
 
 
-class MessageAdapterTestCase(unittest.TestCase):
+class MessageAdapterTestCases(unittest.TestCase):
+
+    python_version = int(''.join([str(i) for i in sys.version_info[0:3]]))
+
     @classmethod
     def setUpClass(cls):
         pass
@@ -15,10 +19,10 @@ class MessageAdapterTestCase(unittest.TestCase):
         pass
 
     def setUp(self):
-        super(MessageAdapterTestCase, self).setUp()
+        super(MessageAdapterTestCases, self).setUp()
 
     def tearDown(self):
-        super(MessageAdapterTestCase, self).tearDown()
+        super(MessageAdapterTestCases, self).tearDown()
 
     def timestamp(self):
         return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -28,21 +32,24 @@ class MessageAdapterTestCase(unittest.TestCase):
         adapter.signature = 'fake_signature'
         adapter.host_name = 'fake_host_name'
         adapter.add_host_record(timestamp=self.timestamp(),
-                                meter_type='fake_meter_type',
+                                meter_name='fake_meter_name',
                                 value='10',
                                 duration=0)
-        adapter.add_instance_record(project_id='fake_project_id',
+        adapter.add_inst_record(project_id='fake_project_id',
                                     user_id='fake_user_id',
-                                    instance_id='fake_instance_id',
+                                    inst_id='fake_inst_id',
                                     timestamp=self.timestamp(),
-                                    meter_type='fake_meter_type',
+                                    meter_name='fake_meter_name',
                                     value='20',
                                     duration=0)
         return adapter.serialize_to_str()
 
     def test_init_empty(self):
         adapter = MessageAdapter()
-        self.assertIsNotNone(adapter._adaptee)
+        if MessageAdapterTestCases.python_version < 270:
+            self.assertTrue(adapter._adaptee is not None)
+        else:
+            self.assertIsNotNone(adapter._adaptee)
 
     def test_init_message(self):
         msg = BulkMessage_pb2.BulkMessage()
@@ -63,38 +70,38 @@ class MessageAdapterTestCase(unittest.TestCase):
         adapter = MessageAdapter()
         self.assertEqual(len(adapter.host_records), 0)
         adapter.add_host_record(timestamp=self.timestamp(),
-                                meter_type='fake_meter_type',
+                                meter_name='fake_meter_name',
                                 value='10',
                                 duration=0)
         self.assertEqual(len(adapter.host_records), 1)
         self.assertEqual(adapter.host_records[0].value, '10')
 
-    def test_add_instance_record(self):
+    def test_add_inst_record(self):
         adapter = MessageAdapter()
-        self.assertEqual(len(adapter.instance_records), 0)
-        adapter.add_instance_record(project_id='fake_project_id',
+        self.assertEqual(len(adapter.inst_records), 0)
+        adapter.add_inst_record(project_id='fake_project_id',
                                     user_id='fake_user_id',
-                                    instance_id='fake_instance_id',
+                                    inst_id='fake_inst_id',
                                     timestamp=self.timestamp(),
-                                    meter_type='fake_meter_type',
+                                    meter_name='fake_meter_name',
                                     value='20',
                                     duration=0)
-        self.assertEqual(len(adapter.instance_records), 1)
-        self.assertEqual(adapter.instance_records[0].value, '20')
+        self.assertEqual(len(adapter.inst_records), 1)
+        self.assertEqual(adapter.inst_records[0].value, '20')
 
     def test_serialize_to_str(self):
         adapter = MessageAdapter()
         adapter.signature = 'fake_signature'
         adapter.host_name = 'fake_host_name'
         adapter.add_host_record(timestamp=self.timestamp(),
-                                meter_type='fake_meter_type',
+                                meter_name='fake_meter_name',
                                 value='10',
                                 duration=0)
-        adapter.add_instance_record(project_id='fake_project_id',
+        adapter.add_inst_record(project_id='fake_project_id',
                                     user_id='fake_user_id',
-                                    instance_id='fake_instance_id',
+                                    inst_id='fake_inst_id',
                                     timestamp=self.timestamp(),
-                                    meter_type='fake_meter_type',
+                                    meter_name='fake_meter_name',
                                     value='20',
                                     duration=0)
         self.assertEqual(isinstance(adapter.serialize_to_str(), str), True)
@@ -105,11 +112,11 @@ class MessageAdapterTestCase(unittest.TestCase):
         self.assertEqual(adapter.signature, 'fake_signature')
         self.assertEqual(adapter.host_name, 'fake_host_name')
         self.assertEqual(adapter.host_records[0].value, '10')
-        self.assertEqual(adapter.instance_records[0].value, '20')
+        self.assertEqual(adapter.inst_records[0].value, '20')
 
     def test_deserialize_to_str_constructor(self):
         adapter = MessageAdapter(self.serialized_data_str())
         self.assertEqual(adapter.signature, 'fake_signature')
         self.assertEqual(adapter.host_name, 'fake_host_name')
         self.assertEqual(adapter.host_records[0].value, '10')
-        self.assertEqual(adapter.instance_records[0].value, '20')
+        self.assertEqual(adapter.inst_records[0].value, '20')

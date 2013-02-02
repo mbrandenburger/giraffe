456069b7a852f948baea2ea9ccfb4e7c5b1ffaee
Marcus <m.brandenburger@tu-bs.de>
2013-02-02 17:40:06 +0100

    Add setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='giraffe',
+    version='0.0.1',
+    url='https://github.com/mbrandenburger/giraffe',
+    author='Marcus Brandenburger',
+    author_email='m.brandenburger@tu-bs.de',
+    install_requires=[],
+)

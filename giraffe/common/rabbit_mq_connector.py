ea53ade2a0de06b5b660c18650c14ae1493a6483
Marcus <m.brandenburger@tu-bs.de>
2013-02-09 20:15:17 +0100

    Ticket #8: Refactor connecto, can now handle username, password, host and port

--- giraffe/common/rabbit_mq_connector.py
+++ giraffe/common/rabbit_mq_connector.py
@@ -1,50 +1,75 @@
 __author__ = 'marcus'
 
+import time
 import pika
+import logging
+from pika.exceptions import AMQPConnectionError
+
+logger = logging.getLogger("agent")
 
 class Connector(object):
-    def __init__(self,host='localhost'):
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host))
-        self.channel = self.connection.channel()
+    def __init__(self, username="guest", password="guest", host="localhost",
+                 port=5672):
+        self.connection = None
+        self.parameters = pika.ConnectionParameters(
+            host=host,
+            port=port,
+            credentials=pika.PlainCredentials(username, password)
+        )
+
+    def connect(self, reconnection_interval=10):
+        try:
+            self.connection = pika.BlockingConnection(self.parameters)
+
+        except AMQPConnectionError:
+            logger.debug("AMQPConnectionException: %s",
+                         AMQPConnectionError)
+            time.sleep(reconnection_interval)
+        except Exception:
+                logger.debug("Error: %s", Exception)
 
     def getConnection(self):
         return self.connection
 
     def getChannel(self):
-        return self.channel
+        return self.connection.channel()
+
+    def isConnected(self):
+        return self.connection.is_open
+
 
 class BasicConsumer(object):
     def __init__(self, connector, queue, exchange, callback):
         self.connector = connector
-        self.channel = connector.channel
         self.exchange = exchange
         self.queue = queue
         self.callback = callback
         self.isConsuming = False
 
     def consume(self):
-        if self.isConsuming == False:
-            self.channel.basic_consume(self._consumer_call, no_ack=True, queue=self.queue)
+        if not self.isConsuming:
+            self.connector.getChannel().basic_consume(self._consumer_call,
+                                                 no_ack=True,
+                                                 queue=self.queue)
             self.isConsuming = True
-            self.channel.start_consuming()
+            self.connector.getChannel().start_consuming()
 
     def stop_consuming(self):
-        self.channel.stop_consuming()
+        self.connector.getChannel().stop_consuming()
         self.isConsuming = False
 
     def _consumer_call(self, ch, method, properties, body):
         self.callback(body)
 
-class BasicProducer(object):
 
+class BasicProducer(object):
     def __init__(self, connector, exchange):
         self.connector = connector
-        self.channel = connector.channel
         self.exchange = exchange
 
-    def send(self,exchange, routing_key, message):
-        self.channel.basic_publish(exchange,
-            routing_key,
-            message,
-            properties=pika.BasicProperties(delivery_mode = 2,)
-        )
+    def send(self, exchange, routing_key, message):
+        self.connector.getChannel().basic_publish(exchange,
+                                             routing_key,
+                                             message,
+                                             properties=pika.BasicProperties(
+                                                 delivery_mode=2, ))

ba24e7e1c589c4976c5e8f699a8b2acf6be24378
Oliver <oliverdm@example>
2013-02-13 21:42:50 +0100

    Added link to host details

--- giraffe_horizon_plugin/giraffe_dashboard/hosts/tables.py
+++ giraffe_horizon_plugin/giraffe_dashboard/hosts/tables.py
@@ -14,7 +14,8 @@ from horizon import tables
 class HostsTable(tables.DataTable):
     id = tables.Column('id', link="horizon:giraffe_dashboard:hosts:detail",
                        verbose_name=_('ID'))
-    name = tables.Column('name', verbose_name=_('Name'))
+    name = tables.Column('name', link="horizon:giraffe_dashboard:hosts:detail",
+                         verbose_name=_('Name'))
     activity = tables.Column('activity', verbose_name=_('Latest Activity'))
 
     class Meta:

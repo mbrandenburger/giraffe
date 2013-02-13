496881541f7b86f119db69aa264c38ab7ff72fe2
fbahr <florian.bahr@gmail.com>
2013-02-13 12:42:37 +0100

    improved output's readability

--- examples/keystone_curl_test.py
+++ examples/keystone_curl_test.py
@@ -2,7 +2,7 @@ __author__ = 'marcus'
 
 import requests
 import simplejson as json
-import keystoneclient.middleware.auth_token as auth
+import keystone.middleware.auth_token as auth
 
 
 USERNAME = ''
@@ -10,9 +10,11 @@ PASSWORD = ''
 TENANT = ''
 ADMIN_TOKEN = ''
 
-"""
-    post user credentials and get token
-"""
+
+print '-----------------------------------\n' \
+      'post user credentials and get token\n' \
+      '-----------------------------------\n'
+
 auth_url = 'http://cloud2.ibr.cs.tu-bs.de:35357/v2.0/tokens'
 auth_header = {'Content-type': 'application/json'}
 auth_data = {'auth':
@@ -21,24 +23,39 @@ auth_data = {'auth':
                        'password': PASSWORD}
                  }
             }
-r = requests.post(url=auth_url, data=json.dumps(auth_data, indent=4),
-                    headers=auth_header)
-print json.dumps(r.json, indent=4)
+print '[AUTH_URL: ', auth_url, ']\n'
+print '<send> auth_data: ', auth_data, '\n'
 
-parsed_data = json.loads(r.text)
-unscoped_token = str(parsed_data['access']['token']['id'])
+r = requests.post(url=auth_url,
+                  data=json.dumps(auth_data, indent=4),
+                  headers=auth_header)
 
-"""
-    get tenents
-"""
+print '<receive> response: ', json.dumps(r.json, indent=4), '\n'
+unscoped_token = str(json.loads(r.text)['access']['token']['id'])
+# print '<receive> unscoped token: ', unscoped_token
+
+
+print '-----------\n' \
+      'get tenants\n' \
+      '-----------\n'
+
 auth_url = 'http://cloud2.ibr.cs.tu-bs.de:5000/v2.0/tenants'
 auth_header = {'X-Auth-Token': unscoped_token}
-r = requests.post(url=auth_url, data={}, headers=auth_header)
-print json.dumps(r.json, indent=4)
 
-"""
-    post user credentials with tenant and get token
-"""
+print '[AUTH_URL: ', auth_url, ']\n'
+print '<send> auth_header: ', auth_header, '\n'
+
+r = requests.post(url=auth_url,
+                  data={},
+                  headers=auth_header)
+
+print '<receive> response: ', json.dumps(r.json, indent=4), '\n'
+
+
+print '-----------------------------------------------\n' \
+      'post user credentials with tenant and get token\n' \
+      '-----------------------------------------------\n'
+
 auth_url = 'http://cloud2.ibr.cs.tu-bs.de:35357/v2.0/tokens'
 auth_header = {'Content-type': 'application/json'}
 auth_data = {'auth':
@@ -46,18 +63,31 @@ auth_data = {'auth':
                   'token': {'id': unscoped_token}
                  }
             }
-r = requests.post(url=auth_url, data=json.dumps(auth_data, indent=4),
-                    headers=auth_header)
-print json.dumps(r.json, indent=4)
 
-parsed_data = json.loads(r.text)
-token = str(parsed_data['access']['token']['id'])
+print '[AUTH_URL: ', auth_url, ']\n'
+print '<send> auth_data: ', auth_data, '\n'
 
-"""
-    check token
-"""
-auth_url = 'http://cloud2.ibr.cs.tu-bs.de:35357/v2.0/tokens/' + token
+r = requests.post(url=auth_url,
+                  data=json.dumps(auth_data, indent=4),
+                  headers=auth_header)
+
+print '<receive> response: ', json.dumps(r.json, indent=4), '\n'
+
+scoped_token = str(json.loads(r.text)['access']['token']['id'])
+# print '<receive> scoped token: ', scoped_token
+
+
+print '-----------\n' \
+      'check token\n' \
+      '-----------\n'
+
+auth_url = 'http://cloud2.ibr.cs.tu-bs.de:35357/v2.0/tokens/' + scoped_token
 auth_header = {'X-Auth-Token': ADMIN_TOKEN}
-r = requests.get(url=auth_url, headers=auth_header)
-print r.text
-print json.dumps(r.json, indent=4)
+
+print '[AUTH_URL: ', auth_url, ']\n'
+print '<send> auth_header: ', auth_header, '\n'
+
+r = requests.get(url=auth_url,
+                 headers=auth_header)
+
+print '<receive> response: ', json.dumps(r.json, indent=4), '\n'

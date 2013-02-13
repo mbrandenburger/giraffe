83c3d3516ad3ffe2629f5e59654ada35d69698cc
Oliver <oliverdm@example>
2013-02-13 20:22:06 +0100

    Renamed form

--- giraffe_horizon_plugin/giraffe_dashboard/forms.py
+++ giraffe_horizon_plugin/giraffe_dashboard/forms.py
@@ -5,7 +5,7 @@ from django.utils import dates
 from horizon import forms
 
 
-class HostAnalysisForm(forms.Form):
+class DateMeterForm(forms.Form):
     """ A simple form for selecting a start date. """
     month = forms.ChoiceField(choices=dates.MONTHS.items())
     year = forms.ChoiceField()
@@ -18,12 +18,14 @@ class HostAnalysisForm(forms.Form):
                 for m in kwargs['meters']:
                     meter_list.append((m.id, m.name))
             del(kwargs['meters'])
-        super(HostAnalysisForm, self).__init__(*args, **kwargs)
+        super(DateMeterForm, self).__init__(*args, **kwargs)
 
         # fill meters
         self.fields['meter'].choices = meter_list
 
         # fill years
-        years = [(year, year) for year in xrange(2009, date.today().year + 1)]
+        today = date.today()
+        years = [(year, year) for year in xrange(today.year - 1,
+                                                 today.year + 1)]
         years.reverse()
         self.fields['year'].choices = years

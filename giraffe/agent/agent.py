311a5cc19636b5565948af4e4e201f86f0080f74
fbahr <florian.bahr@gmail.com>
2013-02-18 21:27:06 +0100

    refactoring: using reflection to instantiate host meter instances

--- giraffe/agent/agent.py
+++ giraffe/agent/agent.py
@@ -3,126 +3,172 @@ __author__ = 'marcus, fbahr'
 import threading
 import time
 from giraffe.agent.host_meter \
-     import Host_CPU_AVG, Host_VIRMEM_Usage, Host_PHYMEM_Usage, Host_UPTIME, \
-            Host_NETWORK_IO
+    import Host_UPTIME, Host_CPU_Load, Host_VIRMEM_Usage, Host_PHYMEM_Usage, \
+           Host_DISK_IO, Host_NETWORK_IO
 from giraffe.agent.instance_meter \
-     import Inst_CPU, Inst_VIRMEM, Inst_PHYMEM, Inst_UPTIME, Inst_DISK_IO, \
-            Inst_NETWORK_IO
+    import Inst_CPU, Inst_VIRMEM, Inst_PHYMEM, Inst_UPTIME, Inst_DISK_IO, \
+           Inst_NETWORK_IO
 from giraffe.agent import publisher
 from giraffe.common.config import Config
 
 import logging
-logger = logging.getLogger("agent")
-config = Config("giraffe.cfg")
-
-_FLUSH_DURATION = config.getint("agent", "duration")
+logger = logging.getLogger('agent')
 
 
 class Agent(object):
-    def __init__(self):
+
+    HOST_METER = {'host_uptime': Host_UPTIME,
+                  'host_cpu_load': Host_CPU_Load,
+                  'host_phymem_usage': Host_PHYMEM_Usage,
+                  'host_virmem_usage': Host_VIRMEM_Usage,
+                  'host_disk_io': Host_DISK_IO,
+                  'host_network_io': Host_NETWORK_IO}
+
+    def __init__(self, config=None):
         """
         Initializes a new Agent.
         """
+
+        # fetch config parameters
+        config = Config(config if config else 'giraffe.cfg')
+
+        # configure agent
         self.timer = False
         self.tasks = []
         self.publisher = publisher.AgentPublisher(self)
 
         # add publisher to tasks
-        self.tasks.append(
-            self.publisher
-        )
+        self.tasks.append(self.publisher)
+
+        # METERS --------------------------------------------------------------
+
+        meters = config.items('agent')
+        for (meter, value) in meters:
+            try:
+                if self.HOST_METER.get(meter):
+                    self.tasks.append(self.HOST_METER[meter](
+                                        getattr(self, '_callback_' + meter),
+                                        int(value)))
+            except ValueError:
+                logging.exception('Host meter %s ommitted due to ValueError: '
+                                  '\'%s\' is not of type \'int\'' % (meter, value))
 
         # HOST METERS ---------------------------------------------------------
 
-        # meter host CPU AVG
-        self.tasks.append(
-            Host_CPU_AVG(self._callback_cpu_avg,
-                         config.getint("agent", "host_loadavg"))
-        )
+        # uptime = config.get('agent', "host_uptime", None)
+        # loadavg = config.get('agent', "host_loadavg", None)
+        # phymem_usage = config.get('agent', "host_phymem_usage", None)
+        # virmem_usage = config.get('agent', "host_virmem_usage", None)
+        # disk_io = config.get('agent', "host_disk_io", None)
+        # network_io = config.get('agent', "host_network_io", None)
 
-        # meter host phy memory
-        self.tasks.append(
-            Host_PHYMEM_Usage(self._callback_phy_mem,
-                              config.getint("agent", "host_phymem_usage"))
-        )
+        # meter host `uptime`
+        # if uptime:
+        #     self.tasks.append(Host_UPTIME(
+        #                         self._callback_uptime,
+        #                         int(uptime)))
 
-        # meter host vir memory
-        self.tasks.append(
-            Host_VIRMEM_Usage(self._callback_vir_mem,
-                              config.getint("agent", "host_virmem_usage"))
-        )
+        # meter host `CPU load`
+        # if loadavg:
+        #     self.tasks.append(Host_CPU_Load(
+        #                         self._callback_cpu_avg,
+        #                         int(loadavg)))
 
-        # meter host uptime
-        self.tasks.append(
-            Host_UPTIME(self._callback_uptime,
-                        config.getint("agent", "host_uptime"))
-        )
+        # meter host `phy mem`
+        # if phymem_usage:
+        #     self.tasks.append(Host_PHYMEM_Usage(
+        #                         self._callback_phy_mem,
+        #                         int(phymem_usage)))
 
-        # meter host network I/O
-        self.tasks.append(
-            Host_NETWORK_IO(self._callback_network_io,
-                            config.getint("agent", "host_network_io"))
-        )
+        # meter host `vir mem`
+        # if virmem_usage:
+        #     self.tasks.append(Host_VIRMEM_Usage(
+        #                         self._callback_vir_mem,
+        #                         int(virmem_usage)))
+
+        # meter host `network I/O`
+        # if network_io:
+        #     self.tasks.append(Host_NETWORK_IO(
+        #                         self._callback_network_io,
+        #                         int(network_io)))
 
         # INSTANCE METERS -----------------------------------------------------
 
         # meter instance cpu utilization
         self.tasks.append(
             Inst_CPU(self._callback_inst_cpu,
-                     config.getint("agent", "inst_cpu"))
+                     config.getint('agent', "inst_cpu"))
         )
 
         # meter instance phy memory
         self.tasks.append(
             Inst_PHYMEM(self._callback_inst_phy_mem,
-                        config.getint("agent", "inst_memory_physical"))
+                        config.getint('agent', "inst_memory_physical"))
         )
 
         # meter instance vir memory
         self.tasks.append(
             Inst_VIRMEM(self._callback_inst_vir_mem,
-                        config.getint("agent", "inst_memory_virtual"))
+                        config.getint('agent', "inst_memory_virtual"))
         )
 
         # meter instance uptime
         self.tasks.append(
             Inst_UPTIME(self._callback_inst_uptime,
-                        config.getint("agent", "inst_uptime"))
+                        config.getint('agent', "inst_uptime"))
         )
 
         # meter instance disk I/O
         self.tasks.append(
             Inst_DISK_IO(self._callback_inst_disk_io,
-                         config.getint("agent", "inst_disk_io"))
+                         config.getint('agent', "inst_disk_io"))
         )
 
         # meter instance network I/O
         self.tasks.append(
             Inst_NETWORK_IO(self._callback_inst_network_io,
-                            config.getint("agent", "inst_network_io"))
+                            config.getint('agent', "inst_network_io"))
         )
 
-    # CB METHODS FOR HOST METERS ----------------------------------------------
+    # CALLBACK METHODS FOR HOST METERS ----------------------------------------
 
-    def _callback_cpu_avg(self, params):
-        self.publisher.add_meter_record('host.loadavg_1m', params[0], 60)
-        self.publisher.add_meter_record('host.loadavg_5m', params[1], 300)
-        self.publisher.add_meter_record('host.loadavg_15m', params[2], 1500)
+    def _callback_host_uptime(self, params):
+        self.publisher.add_meter_record(meter_name='host.uptime',
+                                        meter_value=params,
+                                        meter_duration=0)
 
-    def _callback_phy_mem(self, params):
-        self.publisher.add_meter_record('host.phymem_usage', params[3], 0)
+    def _callback_host_cpu_load(self, params):
+        self.publisher.add_meter_record(meter_name='host.loadavg_1m',
+                                        meter_value=params[0],
+                                        meter_duration=60)
+        self.publisher.add_meter_record(meter_name='host.loadavg_5m',
+                                        meter_value=params[1],
+                                        meter_duration=300)
+        self.publisher.add_meter_record(meter_name='host.loadavg_15m',
+                                        meter_value=params[2],
+                                        meter_duration=1500)
 
-    def _callback_vir_mem(self, params):
-        self.publisher.add_meter_record('host.virmem_usage', params[3], 0)
+    def _callback_host_phymem_usage(self, params):
+        self.publisher.add_meter_record(meter_name='host.phymem_usage',
+                                        meter_value=params[3],
+                                        meter_duration=0)
 
-    def _callback_uptime(self, params):
-        self.publisher.add_meter_record('host.uptime', params, 0)
+    def _callback_host_virmem_usage(self, params):
+        self.publisher.add_meter_record(meter_name='host.virmem_usage',
+                                        meter_value=params[3],
+                                        meter_duration=0)
 
-    def _callback_network_io(self, params):
-        self.publisher.add_meter_record('host.network.io.outgoing.bytes',
-                                        params[0], 0)
-        self.publisher.add_meter_record('host.network.io.incoming.bytes',
-                                        params[1], 0)
+    def _callback_host_disk_io(self, params):
+        print 'hello'
+        pass
+
+    def _callback_host_network_io(self, params):
+        self.publisher.add_meter_record(meter_name='host.network.io.outgoing.bytes',
+                                        meter_value=params[0],
+                                        meter_duration=0)
+        self.publisher.add_meter_record(meter_name='host.network.io.incoming.bytes',
+                                        meter_value=params[1],
+                                        meter_duration=0)
 
     # CB METHODS FOR INSTANCE METERS ------------------------------------------
 
@@ -199,9 +245,9 @@ class Agent(object):
 
     def launch(self):
         """
-        Starts all publisher and all meter tasks.
+        Starts all publisher and meter tasks.
         """
-        global logger
+
         try:
             # start all meter
             for t in self.tasks:
